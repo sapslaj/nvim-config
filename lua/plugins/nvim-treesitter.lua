3a6049cd6c3559874407a02c7eb2e08aeb231fb9
sapslaj <saps.laj@gmail.com>
2024-06-09 20:33:02 -0400

    treesitter oops

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,3 @@
-local Util = require("util")
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -122,6 +120,11 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require('nvim-treesitter.install').prefer_git = true
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup(opts)
+    end,
     legendary = {
       commands = {
         {
