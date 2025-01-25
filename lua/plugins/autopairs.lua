29b5127df6ba06ba9eea99dd67a267f392d032c7
sapslaj <saps.laj@gmail.com>
2025-01-25 01:22:31 -0500

    try autopairs again
    different plugin this time

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+    },
+    config = function(config)
+      local npairs = require("nvim-autopairs")
+      local Rule = require("nvim-autopairs.rule")
+      npairs.setup(config)
+
+      -- placeholder for custom rules
+      -- npairs.add_rules({
+      --   Rule("'")
+      -- })
+    end,
+    keys = {
+      {
+        "<leader>up",
+        function()
+          local npairs = require("nvim-autopairs")
+          npairs.toggle()
+          if npairs.state.disabled then
+            vim.notify("Disabled auto pairs", "info")
+          else
+            vim.notify("Enabled auto pairs", "info")
+          end
+        end,
+      },
+    },
+  },
+}
