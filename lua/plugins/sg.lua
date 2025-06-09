7b17a05383598de870ec254ac206357e0398deb4
Justin Roberson <justin.roberson@angi.com>
2025-06-09 15:34:21 -0400

    i'm bad at Lua

--- lua/plugins/sg.lua
+++ lua/plugins/sg.lua
@@ -7,7 +7,7 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
     },
-    enabled = os.getenv("NVIM_ENABLE_SOURCEGRAPH") ~= "true",
+    enabled = os.getenv("NVIM_ENABLE_SOURCEGRAPH") == "true",
     opts = {
       on_attach = Lsp.on_attach,
     },
