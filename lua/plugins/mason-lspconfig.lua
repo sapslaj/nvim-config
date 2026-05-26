a4c1abe2115b24b4e7a8d85f12107659012d3db2
sapslaj <saps.laj@gmail.com>
2026-05-26 13:15:47 -0400

    rewrite mason-lspconfig config to work with newer nvim

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
   {
     "williamboman/mason-lspconfig.nvim",
-    version = "v1.32.0",
+    version = "v2.2.0",
     dependencies = {
       {
         "williamboman/mason.nvim",
@@ -10,113 +10,12 @@ return {
         "imroc/kubeschema.nvim",
       },
     },
-    opts = {
-      automatic_installation = true,
-      capabilities = {
-        workspace = {
-          fileOperations = {
-            didRename = true,
-            willRename = true,
-          },
-        },
-      },
-      ensure_installed = (function()
-        local function has(...)
-          return vim.fn.executable(...) == 1
-        end
-        local ensure_installed = {
-          "lua_ls",
-        }
-        if has("clang") then
-          table.insert(ensure_installed, "clangd")
-        end
-        if has("elixir") then
-          table.insert(ensure_installed, "elixirls")
-        end
-        if has("go") then
-          table.insert(ensure_installed, "gopls")
-        end
-        if has("helm") then
-          table.insert(ensure_installed, "helm_ls")
-        end
-        if has("npm") then
-          table.insert(ensure_installed, "jsonls")
-          table.insert(ensure_installed, "vtsls")
-        end
-        if has("odin") then
-          table.insert(ensure_installed, "ols")
-        end
-        if has("uv") or has("pip") then
-          table.insert(ensure_installed, "basedpyright")
-        end
-        if has("cargo") or has("rustc") then
-          table.insert(ensure_installed, "rust_analyzer")
-        end
-        if has("zig") then
-          table.insert(ensure_installed, "zls")
-        end
-        return ensure_installed
-      end)(),
-      servers = {
-        basedpyright = {
-          settings = {
-            basedpyright = {
-              analysis = {
-                typeCheckingMode = "basic",
-                -- diagnosticSeverityOverrides = {
-                --   reportAny = false,
-                --   reportUnusedCallResult = false,
-                --   reportMissingTypeArgument = false,
-                --   reportMissingParameterType = false,
-                --   reportUnknownArgumentType = false,
-                --   reportUnknownLambdaType = false,
-                --   reportUnknownMemberType = false,
-                --   reportUnknownParameterType = false,
-                --   reportUnknownVariableType = false
-                -- },
-              },
-            },
-          },
-        },
-        clangd = {
-          keys = {
-            { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-          },
-          root_dir = function(fname)
-            return require("lspconfig.util").root_pattern(
-              "Makefile",
-              "configure.ac",
-              "configure.in",
-              "config.h.in",
-              "meson.build",
-              "meson_options.txt",
-              "build.ninja"
-            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
-              fname
-            ) or require("lspconfig.util").find_git_ancestor(fname)
-          end,
-          capabilities = {
-            offsetEncoding = { "utf-16" },
-          },
-          cmd = {
-            "clangd",
-            "--offset-encoding=utf-16",
-            "--background-index",
-            "--clang-tidy",
-            "--header-insertion=iwyu",
-            "--completion-style=detailed",
-            "--function-arg-placeholders",
-            "--fallback-style=llvm",
-          },
-          init_options = {
-            usePlaceholders = true,
-            completeUnimported = true,
-            clangdFileStatus = true,
-          },
-        },
-        gopls = {},
-        helm_ls = {},
-        jsonls = {},
+    config = function()
+      local function has(...)
+        return vim.fn.executable(...) == 1
+      end
+
+      local config = {
         lua_ls = {
           settings = {
             Lua = {
@@ -139,74 +38,6 @@ return {
                 paramName = "Disable",
                 semicolon = "Disable",
                 arrayIndex = "Disable",
-              },
-            },
-          },
-        },
-        rust_analyzer = {
-          settings = {
-            ['rust-analyzer'] = {
-            },
-          },
-        },
-        terraformls = {
-          filetypes = {
-            "terraform",
-            "terraform-vars",
-            "tf",
-            "tfvars",
-          },
-        },
-        vtsls = {
-          filetypes = {
-            "javascript",
-            "javascriptreact",
-            "javascript.jsx",
-            "typescript",
-            "typescriptreact",
-            "typescript.tsx",
-          },
-          single_file_support = true,
-          settings = {
-            complete_function_calls = true,
-            vtsls = {
-              enableMoveToFileCodeAction = true,
-              autoUseWorkspaceTsdk = true,
-              experimental = {
-                completion = {
-                  enableServerSideFuzzyMatch = true,
-                },
-              },
-            },
-            javascript = {
-              updateImportsOnFileMove = "always",
-            },
-            typescript = {
-              updateImportsOnFileMove = {
-                enabled = "always",
-              },
-              suggest = {
-                completeFunctionCalls = true,
-              },
-              inlayHints = {
-                enumMemberValues = {
-                  enabled = true,
-                },
-                functionLikeReturnTypes = {
-                  enabled = true,
-                },
-                parameterNames = {
-                  enabled = "literals",
-                },
-                parameterTypes = {
-                  enabled = true,
-                },
-                propertyDeclarationTypes = {
-                  enabled = true,
-                },
-                variableTypes = {
-                  enabled = false,
-                },
               },
             },
           },
@@ -243,52 +74,205 @@ return {
             },
           },
         },
-      },
-      setup = {
-        -- clangd = function(_, opts)
-        --   local clangd_ext_opts = require("util").opts("clangd_extensions.nvim")
-        --   require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
-        --   return false
-        -- end,
-        jdtls = function()
-          return true
-        end,
-      },
-      handlers = {},
-    },
-    config = function(_, opts)
-      require("mason-lspconfig").setup(opts)
+      }
 
-      local servers = opts.servers
-      local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-      local capabilities = vim.tbl_deep_extend(
-        "force",
-        {},
-        vim.lsp.protocol.make_client_capabilities(),
-        has_cmp and cmp_nvim_lsp.default_capabilities() or {},
-        opts.capabilities or {}
-      )
+      if has("clang") then
+        config = vim.tbl_deep_extend("force", config, {
+          clangd = {
+            keys = {
+              { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+            },
+            root_dir = function(fname)
+              return require("lspconfig.util").root_pattern(
+                "Makefile",
+                "configure.ac",
+                "configure.in",
+                "config.h.in",
+                "meson.build",
+                "meson_options.txt",
+                "build.ninja"
+              )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+                fname
+              ) or vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
+            end,
+            capabilities = {
+              offsetEncoding = { "utf-16" },
+            },
+            cmd = {
+              "clangd",
+              "--offset-encoding=utf-16",
+              "--background-index",
+              "--clang-tidy",
+              "--header-insertion=iwyu",
+              "--completion-style=detailed",
+              "--function-arg-placeholders",
+              "--fallback-style=llvm",
+            },
+            init_options = {
+              usePlaceholders = true,
+              completeUnimported = true,
+              clangdFileStatus = true,
+            },
+          }
+        })
+      end
 
-      local handlers = vim.tbl_deep_extend("force", {
-        function(server_name)
-          local server_opts = vim.tbl_deep_extend("force", {
-            capabilities = vim.deepcopy(capabilities),
-          }, servers[server_name] or {})
+      if has("elixir") then
+        config = vim.tbl_deep_extend("force", config, {
+          elixirls = {},
+        })
+      end
 
-          if opts.setup[server_name] then
-            if opts.setup[server_name](server_name, server_opts) then
-              return
-            end
-          elseif opts.handlers[server_name] then
-            if opts.handlers[server_name](server_name) ~= false then
-              return
-            end
-          end
-          require("lspconfig")[server_name].setup(server_opts)
-        end,
-      }, opts.handlers or {})
+      if has("go") then
+        config = vim.tbl_deep_extend("force", config, {
+          gopls = {},
+        })
+      end
 
-      require("mason-lspconfig").setup_handlers(handlers)
+      if has("helm") then
+        config = vim.tbl_deep_extend("force", config, {
+          helm_ls = {},
+        })
+      end
+
+      if has("npm") then
+        config = vim.tbl_deep_extend("force", config, {
+          jsonls = {},
+          vtsls = {
+            filetypes = {
+              "javascript",
+              "javascriptreact",
+              "javascript.jsx",
+              "typescript",
+              "typescriptreact",
+              "typescript.tsx",
+            },
+            single_file_support = true,
+            settings = {
+              complete_function_calls = true,
+              vtsls = {
+                enableMoveToFileCodeAction = true,
+                autoUseWorkspaceTsdk = true,
+                experimental = {
+                  completion = {
+                    enableServerSideFuzzyMatch = true,
+                  },
+                },
+              },
+              javascript = {
+                updateImportsOnFileMove = "always",
+              },
+              typescript = {
+                updateImportsOnFileMove = {
+                  enabled = "always",
+                },
+                suggest = {
+                  completeFunctionCalls = true,
+                },
+                inlayHints = {
+                  enumMemberValues = {
+                    enabled = true,
+                  },
+                  functionLikeReturnTypes = {
+                    enabled = true,
+                  },
+                  parameterNames = {
+                    enabled = "literals",
+                  },
+                  parameterTypes = {
+                    enabled = true,
+                  },
+                  propertyDeclarationTypes = {
+                    enabled = true,
+                  },
+                  variableTypes = {
+                    enabled = false,
+                  },
+                },
+              },
+            },
+          },
+        })
+      end
+
+      if has("odin") then
+        config = vim.tbl_deep_extend("force", config, {
+          ols = {}
+        })
+      end
+
+      if has("uv") or has("pip") then
+        config = vim.tbl_deep_extend("force", config, {
+          basedpyright = {
+            settings = {
+              basedpyright = {
+                analysis = {
+                  typeCheckingMode = "basic",
+                  -- diagnosticSeverityOverrides = {
+                  --   reportAny = false,
+                  --   reportUnusedCallResult = false,
+                  --   reportMissingTypeArgument = false,
+                  --   reportMissingParameterType = false,
+                  --   reportUnknownArgumentType = false,
+                  --   reportUnknownLambdaType = false,
+                  --   reportUnknownMemberType = false,
+                  --   reportUnknownParameterType = false,
+                  --   reportUnknownVariableType = false
+                  -- },
+                },
+              },
+            },
+          }
+        })
+      end
+
+      if has("cargo") or has("rustc") then
+        config = vim.tbl_deep_extend("force", config, {
+          rust_analyzer = {
+            settings = {
+              ['rust-analyzer'] = {
+              },
+            },
+          }
+        })
+      end
+
+      if has("terraform") or has("tofu") then
+        config = vim.tbl_deep_extend("force", config, {
+          terraformls = {
+            filetypes = {
+              "terraform",
+              "terraform-vars",
+              "tf",
+              "tfvars",
+            },
+          }
+        })
+      end
+
+      if has("zig") then
+        config = vim.tbl_deep_extend("force", config, {
+          zls = {}
+        })
+      end
+
+      require("mason-lspconfig").setup({
+        ensure_installed = vim.tbl_keys(config),
+        automatic_installation = true,
+        capabilities = {
+          workspace = {
+            fileOperations = {
+              didRename = true,
+              willRename = true,
+            },
+          },
+        },
+      })
+
+      for name, server in pairs(config) do
+        vim.lsp.config(name, server)
+        vim.lsp.enable(name)
+      end
     end,
   },
 }
